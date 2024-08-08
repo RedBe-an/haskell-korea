07ee1b019ebce73f78706ee37165e5eefe08b2b3
Aqouthe_ <161127101+be-noi@users.noreply.github.com>
2024-08-08 12:47:42 +0900

    [feat] vercel analytics updated

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,6 @@ import { Analytics } from "@vercel/analytics/react";
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
-      ...
       <Component {...pageProps} />
       <Analytics />
     </>
