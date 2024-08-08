4da4e5b5b92841de8928ed0f62ed82f98583aa10
Aqouthe_ <161127101+be-noi@users.noreply.github.com>
2024-08-08 10:03:01 +0900

    [init] initialized.

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,69 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/RedBe-an/Haskell-Korea/tree/main",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – Haskell Korea",
+    };
   },
-  chat: {
-    link: 'https://discord.com',
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Language" content="ko" />
+      <meta name="description" content="한국인을 위한 하스켈" />
+      <meta name="og:description" content="한국인을 위한 하스켈" />
+      <meta name="apple-mobile-web-app-title" content="Haskell Korea" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+    </>
+  ),
+  banner: {
+    key: "We-need-your-help",
+    text: (
+      <a href="https://github.com/RedBe-an/Haskell-Korea" target="_blank">
+        Haskell Korea는 여러분들의 도움이 필요합니다. 번역에 힘을 보태 주세요! →
+      </a>
+    ),
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === "separator") {
+        return <span className="cursor-default">{title}</span>;
+      }
+      return <>{title}</>;
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
+  },
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{" "}
+        <a href="https://github.com/RedBe-an/Haskell-Korea" target="_blank">
+          Haskell Korea
+        </a>
+        .
+      </span>
+    ),
   },
-}
+  logo: (
+    <>
+      <span>
+        <strong>Haskell Korea</strong>
+      </span>
+    </>
+  ),
+  project: {
+    link: "https://github.com/RedBe-an/Haskell-Korea",
+  },
+  editLink: {
+    text: "이 페이지 편집하기 →",
+  },
+  feedback: {
+    content: "피드백 주기 →",
+    labels: "피드백",
+  },
+};
 
-export default config
+export default config;
